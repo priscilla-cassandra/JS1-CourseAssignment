a7ddc3bd56ed5964e40edbca3f5ffaa5509484be
priscilla-cassandra <priscilla-92@live.com>
2025-11-13 10:10:35 +0100

    created "go to checkout" button in the cart page

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,7 @@
 import { loadCart } from "./cartUtils.js"
 
 const cartContainer = document.getElementById("cart-container")
+const checkoutButton = document.getElementById("checkout")
 
 const getItems = localStorage.getItem("shoppingCart")
 const retrievedItems = JSON.parse(getItems)
@@ -8,3 +9,8 @@ const retrievedItems = JSON.parse(getItems)
 console.log(retrievedItems)
 
 loadCart(cartContainer)
+
+const goToCheckout = document.createElement("button")
+goToCheckout.classList.add("go-to-checkout-button")
+goToCheckout.textContent = "Go to checkout"
+checkoutButton.appendChild(goToCheckout)
